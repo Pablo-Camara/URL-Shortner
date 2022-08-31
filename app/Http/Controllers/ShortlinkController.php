6e6ecf57e614bba7228ebd918ad2928a80d1645f
Pablo Câmara <pablo.camara.pt@gmail.com>
2022-08-31 02:53:39 +0100

    api /shorten route, ShortlinkController with initial shortlink generation logic

---
+++ app/Http/Controllers/ShortlinkController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shortlink;
+use App\Models\Shortstring;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+
+class ShortlinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // url validation, read below ( about max length )
+        // https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
+
+        $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+
+        $request->validate([
+            'long_url' => 'required|max:2048|regex:'.$regex,
+            'destination_email' => 'required|email:rfc,dns',
+        ]);
+
+        $nextAvailableShortstring = Shortstring::where('is_available', 1)->first();
+
+        if (!$nextAvailableShortstring) {
+            // this should never happen
+            // but in case we ever run out of shortstrings/available links
+            // we should be notified.
+
+            //TODO: Send email notification.
+
+            return new Response(
+                [
+                    'message' => 'No shortlinks available'
+                ],
+                Response::HTTP_SERVICE_UNAVAILABLE
+            );
+        }
+
+        $newShortlink = new Shortlink();
+        $newShortlink->shortstring_id = $nextAvailableShortstring->id;
+        $newShortlink->long_url = $request->input('long_url');
+        $newShortlink->destination_email = $request->input('destination_email');
+        $newShortlink->status_id = Shortlink::STATUS_ACTIVE;
+
+        DB::beginTransaction();
+        try {
+            $newShortlink->save();
+            $nextAvailableShortstring->is_available = 0;
+            $nextAvailableShortstring->save();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+        DB::commit();
+
+        return new Response(
+            [
+                'shortlink' => URL::to('/' . $nextAvailableShortstring->shortstring)
+            ],
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
