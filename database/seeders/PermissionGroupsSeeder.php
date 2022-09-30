d594b63175aac74b8d90189d217b061c79501b54
Pablo Câmara <pablo.camara.pt@gmail.com>
2022-09-30 07:11:32 +0100

    all mighty user can create unlimited links

--- database/seeders/PermissionGroupsSeeder.php
+++ database/seeders/PermissionGroupsSeeder.php
@@ -39,9 +39,13 @@ class PermissionGroupsSeeder extends Seeder
                 'view_shortlinks_total_unique_views' => 1,
                 'create_custom_shortlinks' => 1,
                 'create_shortlinks_with_length_1' => 1,
+                'max_shortlinks_with_length_1' => null,
                 'create_shortlinks_with_length_2' => 1,
+                'max_shortlinks_with_length_2' => null,
                 'create_shortlinks_with_length_3' => 1,
+                'max_shortlinks_with_length_3' => null,
                 'create_shortlinks_with_length_4' => 1,
+                'max_shortlinks_with_length_4' => null,
             ]
         ];
 
