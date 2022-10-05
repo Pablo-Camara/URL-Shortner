937797deb39c124f10f576f4217116f1e126eec4
Pablo Câmara <pablo.camara.pt@gmail.com>
2022-10-05 23:29:47 +0100

    test email command

---
+++ app/Console/Commands/TestSendEmail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\TestEmail;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestSendEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:send-email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Use this command to test if email is sending / delivering, in production.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $testEmails = env('TEST_EMAIL_DESTINATION', null);
+
+        if (is_null($testEmails)) {
+            die('Must set TEST_EMAIL_DESTINATION env variable. (Emails comma separated)');
+        }
+
+        Mail::to(
+            $testEmails
+        )->queue(new TestEmail());
+
+        return Command::SUCCESS;
+    }
+}
---
+++ app/Mail/TestEmail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->text('emails.test_mail')
+            ->subject('Test mail');
+    }
+}
